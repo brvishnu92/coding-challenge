376005272f7534dda73747dc9dea0c7e49cf1a8e
Vishnu Shanker <brvishnu92@gmail.com>
2024-03-12 08:47:21 +1100

    cleanxup api code

--- coding-challenge-api/src/orders.ts
+++ coding-challenge-api/src/orders.ts
@@ -53,16 +53,12 @@ export const getOrders = async (req: any, res: any) => {
 
         const filteredOrders = computeOrders(orders, stores, orderBy, sortBy)
 
-        // const startIndex = (page * recordsPerPage >= filteredOrders.length) ? (filteredOrders.length - recordsPerPage) : page * recordsPerPage
-
         const startIndex = page * recordsPerPage
         const endIndex = startIndex + recordsPerPage
         const finalPageIndex = parseInt((filteredOrders.length / recordsPerPage).toString())
 
-        // res.json({ responses: filteredOrders.slice(startIndex, endIndex), finalPageIndex })
         res.status(200).json({ responses: filteredOrders.slice(startIndex, endIndex), finalPageIndex })
     } catch (e: unknown) {
-        console.error(e)
         const message = e instanceof Error ? e.message : "Unknown error."
         res.status(500).json({ message })
     }
